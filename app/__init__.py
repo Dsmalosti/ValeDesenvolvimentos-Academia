c7b564ae6c7139eddfc2fc3d79619dfdaf0fbb2a
Dsmalosti <Dsmalosti@gmail.com>
2025-11-04 08:27:59 -0300

    configuraçãp app e criação do models User

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from flask_login import LoginManager
+
 from pathlib import Path
 import os
 
@@ -29,6 +31,8 @@ app.config['SECRET_KEY'] = secret_key
 # Inicializa extensões
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 bcrypt = Bcrypt(app)
 
 # Chama a rota inicial
