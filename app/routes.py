315159596d6f35b2f3a8e5980eef75f30e46f4e7
Dsmalosti <Dsmalosti@gmail.com>
2025-11-04 21:36:53 -0300

    Criação da rota para salvar o usuario do instrutor

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,27 @@
 from app import app
 from flask import render_template, url_for, request, redirect
+from flask_login import login_user, logout_user, current_user
 
 from app.models import Aluno
-from app.form import AlunoForm
+from app.form import AlunoForm, UserForm
 
 
 # Rota inicial
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+# Cadastro instrutor
+@app.route('/cadastro-instrutor', methods=['GET', 'POST'])
+def cadastroInstrutor():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = form.save()
+        if current_user.is_authenticated:
+            return redirect(url_for('homepage'))
+        login_user(user, remember=True)
+        return redirect(url_for('homepage'))
+    return render_template('cadastro-instrutor.html', form=form)
 
 # Rota painel administrativo
 @app.route('/painel-adm/')
