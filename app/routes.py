3fe126caf94170c98c500e4a5cb727c8202bf1be
Dsmalosti <Dsmalosti@gmail.com>
2025-11-07 21:34:44 -0300

    Criação login e logout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, url_for, request, redirect
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
+from datetime import datetime
 
 from app.models import Aluno
 from app.form import AlunoForm, UserForm, LoginForm
@@ -8,12 +9,28 @@ from app.form import AlunoForm, UserForm, LoginForm
 
 # Rota inicial
 @app.route('/')
+@login_required
 def homepage():
-    return render_template('index.html')
+    aluno = Aluno.query.all()
+
+    # verifica quantos alunos ativos e manda para notifição
+    ativos = sum(1 for a in aluno if a.status == 'ativo')
+
+    # calcular numero de aniversariantes 
+
+    hoje = datetime.utcnow().date()
+
+    aniversariantes = sum(
+        1 for a in aluno
+        if a.data_nascimento 
+        and a.data_nascimento.day == hoje.day 
+        and a.data_nascimento.month == hoje.month
+    )
+    return render_template('index.html', ativos=ativos, aniversariantes=aniversariantes)
 
 # rota de login
 @app.route('/login/', methods=['GET', 'POST'])
-def loginUser():
+def login():
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -38,17 +55,20 @@ def cadastroInstrutor():
 
 # Rota para sair deslogar
 @app.route('/sair/')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('loginUser'))
+    return redirect(url_for('login'))
 
 # Rota painel administrativo
 @app.route('/painel-adm/')
+@login_required
 def painelAdm():
     return render_template('painel-administrativo.html')
 
 # Rota cadastro aluno
 @app.route('/cadastro-aluno/', methods=['GET', 'POST'])
+@login_required
 def cadastroAluno():
     form = AlunoForm()
     context = {}
